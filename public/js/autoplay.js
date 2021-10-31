53e8b30aaa3eb0ea0a586e31b5178434fe6b7500
Allen Taylor <360137+allentje@users.noreply.github.com>
2021-10-31 23:44:28 +0100

    revert some bad changes

--- public/js/autoplay.js
+++
@@ -1,8 +0,0 @@
-function autoplay(){
-    let confirmation = confirm("Would You Like To AutoPlay Music?");
-    if (confirmation === true) {
-        document.getElementById("audio").play();
-    }
-}
-
-autoplay();
