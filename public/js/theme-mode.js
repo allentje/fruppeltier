657af5dbcf95177b2ed8acd26dae3a25e47fde8d
Allen Taylor <360137+allentje@users.noreply.github.com>
2021-10-31 23:31:48 +0100

    added dark and light mode toggle

---
+++ public/js/theme-mode.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let themeStylesheet = document.getElementById('theme-mode');
+    let themeToggle = document.getElementById('theme-toggle');
+    let sectionTheme = document.getElementById('main');
+
+    themeToggle.addEventListener('click', () => {
+
+        if(themeStylesheet.href.includes('light')){
+            themeStylesheet.href = 'css/theme/dark-theme.css';
+            themeToggle.innerText = 'Light mode';
+            sectionTheme.classList.add('is-dark')
+            sectionTheme.classList.remove('is-light')
+
+            return;
+        }
+
+        themeStylesheet.href = 'css/theme/light-theme.css';
+        themeToggle.innerText = 'Dark mode';
+        sectionTheme.classList.add('is-light');
+        sectionTheme.classList.remove('is-dark')
+
+    })
+})
